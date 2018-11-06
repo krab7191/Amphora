f4fe3cc776b35a469c307868b2841d24aee6e90c
Karsten @ Ubuntu on Dell Latitude e6430s <karstenrabe91@gmail.com>
2018-11-05 21:52:03 -0500

    Format song length properly

--- client/src/components/Row/Row.js
+++ client/src/components/Row/Row.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import './Row.css';
 
-const Row = props => {
+class Row extends React.Component {
 
-    return (
-        <div className={`row song ${props.class}`} >
-            <div className="col-md-2">
-                <img src={props.art[2].url} alt={props.title} className="album-art" />
-            </div>
-            <div className="col-xs-6 container">
-                <p className="row"><strong>{props.title}</strong></p>
-                <p className="row">by <strong>{props.artist}</strong></p>
-                <p className="row">from <em>{props.album}</em></p>
-                <p className="row">0:00 / {(props.length / 60).toFixed(2)}</p>
-            </div>
-        </div >
-    );
+    formatSongLength = seconds => {
+        const mins = Math.floor(seconds / 60);
+        let secs = Math.floor(seconds % 60);
+        if (secs.length === 1) {
+            secs = '0' + secs;
+        }
+        return `${mins}:${secs}`;
+    };
+
+    render() {
+        return (
+            <div className={`row song ${this.props.class}`} >
+                <div className="col-md-2">
+                    <img src={this.props.art[2].url} alt={this.props.title} className="album-art" />
+                </div>
+                <div className="col-xs-6 container">
+                    <p className="row"><strong>{this.props.title}</strong></p>
+                    <p className="row">by <strong>{this.props.artist}</strong></p>
+                    <p className="row">from <em>{this.props.album}</em></p>
+                    <p className="row">0:00 / {this.formatSongLength(this.props.length)}</p>
+                </div>
+            </div >
+        );
+    }
 };
 
 export default Row;
