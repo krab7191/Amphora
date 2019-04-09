92dcadf0ef54ec858a64b0ef1b960cbf90a64341
Karsten Rabe <karstenrabe91@gmail.com>
2019-04-09 11:42:03 -0400

    Redo audio controls, streamline CSS

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,67 +1,70 @@
-import React, { Component } from 'react';
-import Slider, { createSliderWithTooltip } from 'rc-slider';
-import 'rc-slider/assets/index.css';
-import './Footer.css';
+import React, { Component } from "react";
+import Slider, { createSliderWithTooltip } from "rc-slider";
+import "rc-slider/assets/index.css";
+import "./Footer.css";
 
-const FontAwesome = require('react-fontawesome');
+const FontAwesome = require("react-fontawesome");
 const TooltipSlider = createSliderWithTooltip(Slider);
 
-
 class Footer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            volumeIcon: "volume-down",
-            volume: 50
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      volumeIcon: "volume-down",
+      volume: 50
     };
+  }
 
-    volumeHandler = e => {
-        if (e === 0) {
-            this.setState({
-                volumeIcon: 'volume-off'
-            });
-        }
-        else if (e >= 60) {
-            this.setState({
-                volumeIcon: 'volume-up'
-            });
-        }
-        else {
-            this.setState({
-                volumeIcon: 'volume-down'
-            });
-        }
+  volumeHandler = e => {
+    if (e === 0) {
+      this.setState({
+        volumeIcon: "volume-mute"
+      });
+    } else if (e >= 60) {
+      this.setState({
+        volumeIcon: "volume-up"
+      });
+    } else {
+      this.setState({
+        volumeIcon: "volume-down"
+      });
     }
+  };
 
-    render() {
-        console.log(this.props.playing);
+  render() {
+    console.log(this.props.playing);
 
-        return (
-            <div
-                className="footer"
-            >
-                <div className="hCenter">
-                    <div className="controlBox" onClick={this.props.playPause}>
-                        <FontAwesome name={this.props.playing} onClick={this.props.playPause} />
-                    </div>
-                    <div className="controlBox" onClick={this.props.nextSong}>
-                        <FontAwesome name="step-forward" onClick={this.props.nextSong} />
-                    </div>
-                    <div className="controlBox" id="volume-button">
-                        <FontAwesome name={this.state.volumeIcon} />
-                    </div>
-                    <div id="volume-slider" >
-                        <TooltipSlider
-                            defaultValue={this.state.volume}
-                            onChange={this.volumeHandler}
-                            vertical={true}
-                        />
-                    </div>
-                </div>
-            </div>
-        )
-    }
-};
+    return (
+      <div className="footer">
+        <div className="hCenter">
+          <div className="controlBox" onClick={this.props.playPause}>
+            <FontAwesome
+              name={this.props.playing}
+              onClick={this.props.playPause}
+              size="2x"
+            />
+          </div>
+          <div className="controlBox" onClick={this.props.nextSong}>
+            <FontAwesome
+              name="step-forward"
+              onClick={this.props.nextSong}
+              size="2x"
+            />
+          </div>
+          <div className="controlBox" id="volume-button">
+            <FontAwesome name={this.state.volumeIcon} size="2x" />
+          </div>
+          <div id="volume-slider">
+            <TooltipSlider
+              defaultValue={this.state.volume}
+              onChange={this.volumeHandler}
+              vertical={true}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Footer;
