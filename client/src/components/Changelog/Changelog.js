3ba58095bd67c9067a9ca024ef54d386ab333814
Karsten Rabe <karstenrabe91@gmail.com>
2019-12-08 16:41:42 -0500

    Update changelog

--- client/src/components/Changelog/Changelog.js
+++ client/src/components/Changelog/Changelog.js
@@ -8,27 +8,33 @@ class Changelog extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      logs: [ 
+      logs: [
+        {
+          version: "0.48.02",
+          changes: [
+            "Upgrade packages to address security vulnerabilities",
+          ],
+        },
         {
           version: "0.48.01",
           changes: [
-            "Bugfixes for handling end of queue"
-          ]
+            "Bugfixes for handling end of queue",
+          ],
         },
         {
           version: "0.48",
           changes: [
             "Make station dropdown responsive",
             "Minor media query tweaks to header",
-            "Prevent reload when clicking current station"
-          ]
+            "Prevent reload when clicking current station",
+          ],
         },
         {
           version: "0.47",
           changes: [
             "Trigger show volume slider when using up/down arrows to adjust volume",
-            "Bugfix for albumArt url not found"
-          ]
+            "Bugfix for albumArt url not found",
+          ],
         },
         {
           version: "0.46",
@@ -37,47 +43,47 @@ class Changelog extends Component {
             "Prevent overflow song details wrapping",
             "Audio volume slider bugfixes",
             "Prompt user to retry on audio fetch error",
-            "Add native scroll bounce to legalBox"
-          ]
+            "Add native scroll bounce to legalBox",
+          ],
         },
         {
           version: "0.45",
           changes: [
             "Make up / down keys change volume",
-            "Fix arrow keybindings, and other minor bugfixes"
-          ]
+            "Fix arrow keybindings, and other minor bugfixes",
+          ],
         },
         {
           version: "0.44.02",
-          changes: ["Bugfixes to displaying changeLog", "Audit dependencies"]
+          changes: ["Bugfixes to displaying changeLog", "Audit dependencies"],
         },
         {
           version: "0.44.01",
-          changes: ["Prevent page scroll on spacebar keydown"]
+          changes: ["Prevent page scroll on spacebar keydown"],
         },
         {
           version: "0.44",
           changes: [
             "Double size of audio controls",
             "Remove borders and other minor style tweaks",
-            "Add audio control keybindings for desktop."
-          ]
+            "Add audio control keybindings for desktop.",
+          ],
         },
         {
           version: "0.43.01",
           changes: [
-            "Fix media control bug. 'Play', 'skip', and 'volume' icons are back!"
-          ]
+            "Fix media control bug. 'Play', 'skip', and 'volume' icons are back!",
+          ],
         },
         {
           version: "0.43",
           changes: [
             "Changelog added. Upon every new release, changelog will show unless user specifies to never show changelog again.",
             "Migrate to Bootstrap 4. All components were updated to reflect the changes from BS 3 to BS 4.",
-            "Removed unused 'Settings' tab and corresponding gear icon"
-          ]
-        }
-      ]
+            "Removed unused 'Settings' tab and corresponding gear icon",
+          ],
+        },
+      ],
     };
   }
 
